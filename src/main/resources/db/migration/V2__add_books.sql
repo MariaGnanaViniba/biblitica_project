f393448c984c538619aeea4e8d9a6d309a3cbdd3
Amit Kumar <amit.kumar.thoughtworks@aegonlife.com>
2021-11-29 18:11:32 +0530

    creates initial biblioteca application

---
+++ src/main/resources/db/migration/V2__add_books.sql
@@ -0,0 +1,4 @@
+insert into books (isbn, title, author, publisher, year_of_publication)
+values ('195153448', 'Classical Mythology', 'Mark P. O. Morford', 'Oxford University Press', 2002),
+       ('2005018', 'Clara Callan', 'Richard Bruce Wright', 'HarperFlamingo Canada', 2001);
+
