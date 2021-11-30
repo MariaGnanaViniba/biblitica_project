669995135dddf4a93cebe36fde1f2eb4c7ab0521
Amit Kumar <amit.kumar.thoughtworks@aegonlife.com>
2021-11-30 11:34:12 +0530

    adds more books

--- src/main/resources/db/migration/V2__add_books.sql
+++ src/main/resources/db/migration/V2__add_books.sql
@@ -1,4 +1,7 @@
 insert into books (isbn, title, author, publisher, year_of_publication)
 values ('195153448', 'Classical Mythology', 'Mark P. O. Morford', 'Oxford University Press', 2002),
-       ('2005018', 'Clara Callan', 'Richard Bruce Wright', 'HarperFlamingo Canada', 2001);
+       ('2005018', 'Clara Callan', 'Richard Bruce Wright', 'HarperFlamingo Canada', 2001),
+       ('393045218', 'The Mummies of Urumchi', 'E. J. W. Barber', 'W. W. Norton &amp; Company', 1999),
+       ('671870432', 'PLEADING GUILTY', 'Scott Turow', 'Audioworks', 1993),
+       ('679425608', 'Under the Black Flag The Romance and the Reality of Life Among the Pirates', 'David Cordingly', 'Random House', 1996);
 
