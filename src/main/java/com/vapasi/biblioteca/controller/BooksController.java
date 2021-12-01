ca09cf157a30b2ae1a64b8c56034a280d9897cef
HarshadaDawande <HarshadaDawande@gmail.com>
2021-12-01 14:23:46 +0530

    Added new endpoint

--- src/main/java/com/vapasi/biblioteca/controller/BooksController.java
+++ src/main/java/com/vapasi/biblioteca/controller/BooksController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("api/v1/books")
+@RequestMapping("/books")
 public class BooksController {
 
     BooksService booksService;
@@ -25,11 +25,14 @@ public class BooksController {
         this.booksService = booksService;
     }
 
+    @GetMapping("/welcome")
+    public String getGreetings() {
+        return "Welcome Biblioteca";
+    }
+
     @GetMapping("/")
     public ResponseEntity<List<BookDto>> getMovies() {
         List<BookDto> allBookDto = booksService.getAllBooks();
         return ResponseEntity.ok().body(allBookDto);
     }
-
-
 }
