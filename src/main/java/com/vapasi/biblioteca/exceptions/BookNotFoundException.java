af0a29cb1262caa8d57bb23eaf0595bbf37c20f2
HarshadaDawande <HarshadaDawande@gmail.com>
2021-12-02 20:38:58 +0530

    Added exceptions

---
+++ src/main/java/com/vapasi/biblioteca/exceptions/BookNotFoundException.java
@@ -0,0 +1,4 @@
+package com.vapasi.biblioteca.exceptions;
+
+public class BookNotFoundException extends RuntimeException{
+}
---
+++ src/main/java/com/vapasi/biblioteca/exceptions/CustomerNotFoundException.java
@@ -0,0 +1,4 @@
+package com.vapasi.biblioteca.exceptions;
+
+public class CustomerNotFoundException extends RuntimeException{
+}
