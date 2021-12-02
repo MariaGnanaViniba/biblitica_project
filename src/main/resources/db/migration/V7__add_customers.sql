96a4a76472fcb5a856e23513ba9044d911194505
HarshadaDawande <HarshadaDawande@gmail.com>
2021-12-02 11:05:23 +0530

    Added scripts for customer and mapping table

---
+++ src/main/resources/db/migration/V7__add_customers.sql
@@ -0,0 +1,8 @@
+insert into customer (customername,email,phone)
+values ('Maria', 'm@gmail.com', ''),
+       ('Nivetha', 'nivi@gmail.com', ''),
+       ('Nandini', 'n@gmail.com', ''),
+       ('Harshada', 'h@gmail.com', ''),
+       ('Amit', 'a@gmail.com', ''),
+       ('Nithya', 'nn@gmail.com', '');
+
