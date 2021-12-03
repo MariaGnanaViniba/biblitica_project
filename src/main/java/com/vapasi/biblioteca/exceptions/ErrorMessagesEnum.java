eef0e88ba78a302ec46528f49296d7511c1d9223
HarshadaDawande <HarshadaDawande@gmail.com>
2021-12-03 11:30:58 +0530

    Enum for error messages

--- src/main/java/com/vapasi/biblioteca/exceptions/ErrorMessagesEnum.java
+++ src/main/java/com/vapasi/biblioteca/exceptions/ErrorMessagesEnum.java
@@ -1,4 +1,19 @@
 package com.vapasi.biblioteca.exceptions;
 
 public enum ErrorMessagesEnum {
+    BookNotFound("Book not found, please try again."),
+    CustomerNotFound("Customer not found, please try again."),
+    BookAlreadyIssued("This book is already issued to you."),
+    BookSuccessfullyIssued("Book is successfully issued."),
+    ImproperDetails("Please provide proper details, try again.");
+    private String message;
+
+    private ErrorMessagesEnum(String s) {
+        message = s;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
 }
